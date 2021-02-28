5fabf26e2dcb5407afe02bad01efbf886d8a2d5c
Peter Ting <pting1995@gmail.com>
2021-02-27 21:33:18 -0800

    finished landing page and styling elements

--- portfolio-mk4/src/components/AboutMeCard/index.js
+++ portfolio-mk4/src/components/AboutMeCard/index.js
@@ -10,13 +10,13 @@ function AboutMe() {
                 <p className="important">University of California, Riverside</p>
                 <div>
                     <a href="https://www.linkedin.com/in/pting002/" target="_blank">
-                        <i className="fa fa-linkedin contact-icon" style="font-size:40px; padding: 10px;"></i>
+                        <i className="fa fa-linkedin contact-icon about-icon"></i>
                     </a>
                     <a href="https://github.com/Pting1995" target="_blank">
-                        <i className="fa fa-github contact-icon" style="font-size:40px; padding: 10px;"></i>
+                        <i className="fa fa-github contact-icon about-icon"></i>
                     </a>
                     <a href="./assets/Resume.pdf" target="_blank">
-                        <i className="far fa-file contact-icon" style="font-size:40px; padding: 10px;"></i>
+                        <i className="far fa-file contact-icon about-icon"></i>
                     </a>
                 </div>
             </div>
